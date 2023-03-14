4977313daeb42e36a5e9718cbb04101757536142
IA7308 <naziqnaz13@gmail.com>
2023-03-14 17:07:00 +0700

    menambahkan class PosisiKarakterGame
    menambahkan class PosisiKarakterGame

--- JurnalMod4_1302210104/Program.cs
+++ JurnalMod4_1302210104/Program.cs
@@ -23,12 +23,81 @@
 
         return KodeBuah[(int)Buah];
     }
+}
+
+public class PosisiKarakterGame
+{
+    public enum State
+    {
+        Tengkurap,
+        Jongkok,
+        Berdiri,
+        Terbang
+    }
 
     public static void Main()
     {
         //KodeBuah buah = new KodeBuah();
-        Console.WriteLine("Nama Buah " + KodeBuah.namaBuah.Blackberry + " Kode Buah " +KodeBuah.getKodeBuah(KodeBuah.namaBuah.Blackberry));
+        Console.WriteLine("Nama Buah " + KodeBuah.namaBuah.Blackberry + " Kode Buah " + KodeBuah.getKodeBuah(KodeBuah.namaBuah.Blackberry));
 
+        
+        State Kondisi = State.Tengkurap;
+        Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+        string Perintah;
+        while (Kondisi == State.Tengkurap) { 
+            Perintah = Console.ReadLine();
+            if(Perintah == "TombolW")
+            {
+                Kondisi = State.Jongkok;
+                Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                while (Kondisi == State.Jongkok) {
+                    Perintah = Console.ReadLine();
+                    if (Perintah == "TombolW")
+                    {
+                        Kondisi= State.Berdiri;
+                        Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                        while (Kondisi == State.Berdiri) { 
+                            Perintah = Console.ReadLine();
+                            if(Perintah == "TombolW")
+                            {                               
+                                Kondisi= State.Terbang;
+                                Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                                Console.WriteLine("posisi take off");
+                                while (Kondisi == State.Terbang)
+                                {
+                                    Perintah = Console.ReadLine();
+                                    if(Perintah == "TombolS")
+                                    {                                     
+                                        Kondisi = State.Berdiri;
+                                        Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                                    }
+                                    else if(Perintah == "TombolX")
+                                    {                                        
+                                        Kondisi = State.Jongkok;
+                                        Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                                        Console.WriteLine("posisi landing");
+                                    }
+                                }
+                            }
+                            else if (Perintah == "TombolS"){
+                                Kondisi = State.Jongkok;
+                                Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                            }
+                        }
+                    }
+                    else if (Perintah == "TombolS")
+                    {
+                        Kondisi= State.Tengkurap;
+                        Console.WriteLine("kondisi saat ini sedang : " + Kondisi);
+                    }
+                    
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
 
